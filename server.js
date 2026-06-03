d7450cc74ae79a89779c8ed0b1aac29683a9d630
IsaacEswa <isaaceswa@gmail.com>
2026-06-03 13:00:15 +0200

    set up server.js and the work enviroment #2

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+import { Liquid } from 'liquidjs';
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', './views')
+
+const baseURL = ''
+
+app.get('/', async function (request, response) {
+
+    response.render('index.liquid')
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("Deze pagina bestaat niet")
+})
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+    console.log(`Application started on http://localhost:${app.get('port')}`)
+})
