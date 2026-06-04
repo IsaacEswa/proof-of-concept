2425548df6816c640f10a336011ee406e3eba013
IsaacEswa <isaaceswa@gmail.com>
2026-06-04 12:05:10 +0200

    #4 added exhibits_sections API & DataResponse to server.js and inserted JSON in details

--- server.js
+++ server.js
@@ -7,11 +7,25 @@ const engine = new Liquid();
 app.engine('liquid', engine.express());
 app.set('views', './views')
 
-const baseURL = ''
+// Endpoints:
+// https://fdnd-agency.directus.app/items/teylers_museum_exhibits/1 | Topstuk
+// https://fdnd-agency.directus.app/items/teylers_museum_exhibits_sections | Timeline items
+// https://fdnd-agency.directus.app/items/teylers_museum_persons | Creators
+
+const baseURL = 'https://fdnd-agency.directus.app/items/teylers_museum_'
+const timelineItem_fields = 'title, id, slug, exhibit, cover, content_block.*, start_year, end_year, era, summary'
 
 app.get('/', async function (request, response) {
+    const params = new URLSearchParams()
+    params.set('fields', timelineItem_fields)
+    // params.set('sort', '-date')
 
-    response.render('index.liquid')
+    const timelineDataResponse = await fetch(baseURL + 'exhibits_sections' + '?' + params.toString())
+    const timelineDataResponseJSON = await timelineDataResponse.json()
+
+    console.log(timelineDataResponseJSON)
+
+    response.render('index.liquid', { timelineData: timelineDataResponseJSON.data })
 })
 
 app.use((req, res, next) => {
