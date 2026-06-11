5419afcfc7f290e6438ee97cfa14415053679400
IsaacEswa <isaaceswa@gmail.com>
2026-06-11 09:40:29 +0200

    #10  changed POST query parameter in redirect so it matches with GET parameter & cleaned code

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.set('views', './views')
 // https://fdnd-agency.directus.app/items/teylers_museum_quiz_answers| Quiz answers
 
 const baseURL = 'https://fdnd-agency.directus.app/items/teylers_museum_'
-// const timelineItem_fields = 'title, id, slug, exhibit, cover.*, content_blocks, start_year, end_year, era, summary'
 
 app.get('/', async function (request, response) {
     const answered = request.query.answered
@@ -36,10 +35,6 @@ app.get('/', async function (request, response) {
 
     const sectionsJSON = await sectionsResponse.json()
 
-    // console.log(sectionsJSON)
-    // console.dir(sectionsJSON, { depth: null })
-    // console.log(request.query)
-
     const sections = {
         sections: sectionsJSON.data,
         answered: request.query.answered,
@@ -50,35 +45,20 @@ app.get('/', async function (request, response) {
 })
 
 app.post('/quiz/answer', async (request, response) => {
-    // console.log('POST /quiz/answer hit!')
-    // console.log(request.body)
-
-
     const { questionId, answerKey } = request.body
 
-    // 2.1 haal vraag opnieuw op
     const questionResponse = await fetch(
         `${baseURL}quiz_questions/${questionId}?fields=*,options.*`
         // BIJV: https://fdnd-agency.directus.app/items/teylers_museum_quiz_questions/1?fields=*,options.*
     )
-
     const questionJSON = await questionResponse.json()
 
-    // check antwoord
     const selectedOption = questionJSON.data.options.find(
         option => option.key === answerKey
     )
 
     const isCorrect = selectedOption?.is_correct === true
 
-    // console.log({
-    //     questionId,
-    //     answerKey,
-    //     isCorrect
-    // })
-
-
-    // 2.2 fetchen en posten
     await fetch(baseURL + 'quiz_answers', {
         method: 'POST',
         headers: {
@@ -93,18 +73,9 @@ app.post('/quiz/answer', async (request, response) => {
         })
     })
 
-    // console.log(request.body)
-
-
-
-
-    // 2.3 redirect terug
     response.redirect(
-        '/?question=' + questionId + '&correct=' + isCorrect
+        '/?answered=' + questionId + '&correct=' + isCorrect
     )
-
-    // console.log(request.query)
-
 })
 
 
